f8c28381f4be3996c22e8c2058227c230f60efed
AlexandruC97 <63367791+AlexandruC97@users.noreply.github.com>
2024-03-04 21:15:31 +0200

    Session 4 class code

--- src/com/itschool/Session4/Course/Session4Class.java
+++ src/com/itschool/Session4/Course/Session4Class.java
@@ -3,5 +3,21 @@ package com.itschool.Session4.Course;
 public class Session4Class {
     public static void main(String[] args) {
         System.out.println("Session 4 class!");
+
+        String firstName = "Alex";
+        String lastName = "Cristescu";
+
+        System.out.println(firstName + lastName);
+
+        firstName += lastName;
+        System.out.println(firstName);
+
+        boolean trueVar = true;
+
+        System.out.println("trueVar is " + !trueVar);
+
+        boolean falseVar = false;
+
+        System.out.println("falseVar is " + !falseVar);
     }
 }
